549398346ab5ee502fa26f9abf1e937410b87625
David Ben-Yaakov <david@tangogolfdigital.com>
2020-07-17 14:33:07 -0500

    Removed dir-locals as it can be generated

--- .dir-locals.el
+++
@@ -1,8 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((swift-mode . 
-  ((lsp-sourcekit-extra-args . (
-	 "-Xlinker" "-L" "-Xlinker" "The specified library path was not found '/usr/local/lib/merlin/Scenes-1.0.11/Scenes/.build/debug'" "-Xswiftc" "-I" "-Xswiftc" "The specified library path was not found '/usr/local/lib/merlin/Scenes-1.0.11/Scenes/.build/debug'" 
-
-)))))
